57ca0e23e3ba83e32c8d36925c39005749bea735
azosa <40138353+azosa@users.noreply.github.com>
2019-02-05 18:50:40 +0100

    Movielist

--- Movie Search Engine/Movie Search Engine/UIController/UserControl1.xaml.cs
+++ Movie Search Engine/Movie Search Engine/UIController/UserControl1.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Search_Engine_Library;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +18,19 @@ using System.Windows.Shapes;
 
 namespace Movie_Search_Engine.UIController
 {
+    public class ImageClass
+
+    {
+
+        public int Id { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public byte[] ImageToByte { get; set; }
+
+    }
+ 
+
     /// <summary>
     /// Logika interakcji dla klasy UserControl1.xaml
     /// </summary>
@@ -23,8 +39,74 @@ namespace Movie_Search_Engine.UIController
         public UserControl1()
         {
             InitializeComponent();
-        }
 
-      
+
+            DBConnect connection = new DBConnect();
+            DataManipulator movies = new DataManipulator();
+            //DataManipulator.ShowTable(DataManipulator.GetMovieData());
+
+            //Image img = new System.Windows.Controls.Image();
+
+
+
+            //title.Content = movies.MovieList[1].Title;
+            //Polecane.Children.Add(title);
+       
+        StackPanel stack = new System.Windows.Controls.StackPanel();
+                stack.HorizontalAlignment = HorizontalAlignment.Center;
+                Polecane.Children.Add(stack);
+
+            for (int i = 0; i < movies.MovieList.Count; i++)
+            { 
+                Image img = new Image();
+                ImageClass images = new ImageClass();
+                var result = movies.MovieList[i].Poster;
+                Stream StreamObj = new MemoryStream(result);
+                BitmapImage BitObj = new BitmapImage();
+                BitObj.BeginInit();
+                BitObj.StreamSource = StreamObj;
+                BitObj.EndInit();
+                img.Source = BitObj;
+                img.Height = 100;
+                img.Width = 100;
+
+                Label title = new System.Windows.Controls.Label();
+                Label rlsDate = new System.Windows.Controls.Label();
+                Label genre = new System.Windows.Controls.Label();
+                Label lng = new System.Windows.Controls.Label();
+                Label rnTime = new System.Windows.Controls.Label();
+                Label price = new System.Windows.Controls.Label();
+              
+                title.Content = "Title" + movies.MovieList[i].Title;
+                rlsDate.Content = "Realise date: " +movies.MovieList[i].Releasedate;
+                genre.Content = "Genre" + movies.MovieList[i].Genre;
+                lng.Content = "Language" + movies.MovieList[i].Language;
+                rnTime.Content = "Runtime"+ movies.MovieList[i].Runtime;
+                price.Content = "Price: " + movies.MovieList[i].Price;
+
+                stack.Children.Add(img);
+                stack.Children.Add(price);
+                stack.Children.Add(title);
+                stack.Children.Add(rlsDate);
+                stack.Children.Add(lng);
+                stack.Children.Add(rnTime);
+            }
+
+
+        }
+            //foreach (Movie m in movies.MovieList)
+            //{
+            //    title.Content = m.Title;
+
+
+            //    Polecane.Children.Add(img);
+            //    Polecane.Children.Add(title);
+            //}
+
+
+
     }
+
+
+    
 }
