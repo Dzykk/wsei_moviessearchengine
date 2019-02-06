0378a39ef60f2b00d7a50cddf1a745e935015549
azosa <40138353+azosa@users.noreply.github.com>
2019-02-06 10:16:28 +0100

    .

--- Movie Search Engine/Movie Search Engine/UIController/UserControl6.xaml.cs
+++ Movie Search Engine/Movie Search Engine/UIController/UserControl6.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Search_Engine_Library;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +27,33 @@ namespace Movie_Search_Engine.UIController
         {
             InitializeComponent();
         }
+
+        
+        
+
+
+        private void addMovie_Click(object sender, RoutedEventArgs e)
+        {
+            DBConnect dbcon = new DBConnect();
+            SqlConnection con = dbcon.Connect();
+            con.Open();
+            string userquery =
+                "MERGE [dbo].[Movie] AS tab " +
+                "USING (SELECT @Title AS Title, @Price AS Price, @ReleaseDate AS ReleaseDate, @Genre AS Genre, @Language AS [Language], @Runtime AS Runtime) AS src " +
+                "ON tab.Title = src.Title AND tab.ReleaseDate = src.ReleaseDate " +
+                "WHEN NOT MATCHED THEN " +
+                "INSERT (Title, Price, ReleaseDate, Genre, [Language], Runtime)" +
+                "VALUES (src.Title, src.Price, src.ReleaseDate, src.Genre, src.[Language], src.Runtime) " +
+                "END";
+            SqlCommand cmd = new SqlCommand(userquery, con);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@Title", txt1.Text);
+            cmd.Parameters.AddWithValue("@Price", txt2.Text);
+            cmd.Parameters.AddWithValue("@ReleaseDate", txt3.Text);
+            cmd.Parameters.AddWithValue("@Genre", txt4.Text);
+            cmd.Parameters.AddWithValue("@Language", txt5.Text);
+            cmd.Parameters.AddWithValue("@Runtime", txt6.Text);
+        }
     }
+    
 }
