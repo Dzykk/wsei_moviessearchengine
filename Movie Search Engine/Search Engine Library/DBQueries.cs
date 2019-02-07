9304687f2f905dbf5b24c95acfee43ba4785288f
Jakub Juszczak <jakub.juszczak@microsoft.wsei.edu.pl>
2019-02-07 17:08:00 +0100

    Documentation - DBQueries

--- Movie Search Engine/Search Engine Library/DBQueries.cs
+++ Movie Search Engine/Search Engine Library/DBQueries.cs
@@ -10,6 +10,17 @@ namespace Search_Engine_Library
 {
    public class DBQueries
     {
+        /// <summary>
+        /// Metoda statyczna CheckExistingUser
+        /// Sprawdza, czy podany użytkownik istnieje już w bazie i zwraca wartość 1, jeśli istnieje i 0, jeśli nie istnieje.
+        /// </summary>
+        /// <remarks>
+        /// Wykorzystując połączenie z bazą danych SQL, wysyła kwerendę liczącą ilość wierszy zawierających podany przez użytkownika login. Jeśli zwróci wartość większą niż 0, to znaczy, że 
+        /// użytkownik o podanym loginie już istnieje.
+        /// </remarks>
+        /// <param name="con">Odpowiada za połączenie z bazą danych, w formacie SqlConnection</param>
+        /// <param name="username">Odpowiada za login użytkownika, w formacie string</param>
+        /// <returns>Wartość 0 lub 1, w zależności od tego, czy użytkownik nie istnieje lub istnieje.</returns>
         public static int CheckExistingUser(SqlConnection con, string username)
         {
             SqlCommand cmd = new SqlCommand();
@@ -21,6 +32,16 @@ namespace Search_Engine_Library
             return usercheck;
         }
 
+        /// <summary>
+        /// Metoda statyczna RegisterNewUser
+        /// Pobiera dane podane przez użytkownika aplikacji i tworzy nowego użytkownika.
+        /// </summary>
+        /// <remarks>
+        /// Wykorzystując połączenie z bazą danych SQL, wysyła polecenie INSERT do bazy danych, tworząc w bazie nowego użytkownika korzystając z podanych danych.
+        /// </remarks>
+        /// <param name="con">Odpowiada za połączenie z bazą danych, w formacie SqlConnection</param>
+        /// <param name="username">Odpowiada za login użytkownika, w formacie string</param>
+        /// <param name="password">Odpowiada za hasło użytkownika, w formacie string</param>
         public static void RegisterNewUser(SqlConnection con, string username, string password)
         {
             SqlCommand cmd = new SqlCommand();
@@ -32,6 +53,18 @@ namespace Search_Engine_Library
             cmd.ExecuteNonQuery();
         }
 
+        /// <summary>
+        /// Metoda statyczna UserLogIn
+        /// Sprawdza, czy podany użytkownik istnieje w bazie i zwraca wartość 1, jeśli istnieje i 0, jeśli nie istnieje.
+        /// </summary>
+        /// <remarks>
+        /// Wykorzystując połączenie z bazą danych SQL, wysyła kwerendę liczącą ilość wierszy zawierających podany przez użytkownika login i hasło. Jeśli zwróci wartość większą niż 0, to znaczy, że 
+        /// użytkownik o podanym loginie istnieje i może się zalogować.
+        /// </remarks>
+        /// <param name="con">Odpowiada za połączenie z bazą danych, w formacie SqlConnection</param>
+        /// <param name="username">Odpowiada za login użytkownika, w formacie string</param>
+        /// <param name="password">Odpowiada za hasło użytkownika, w formacie string</param>
+        /// <returns>Wartość 0 lub 1, w zależności od tego, czy użytkownik nie istnieje lub istnieje.</returns>
         public static int UserLogIn(SqlConnection con, string username, string password)
         {         
             SqlCommand cmd = new SqlCommand();
@@ -44,6 +77,17 @@ namespace Search_Engine_Library
             return usercheck;
         }
 
+        /// <summary>
+        /// Metoda statyczna CheckExistingMovie
+        /// Sprawdza, czy podany film istnieje już w bazie i zwraca wartość 1, jeśli istnieje i 0, jeśli nie istnieje.
+        /// </summary>
+        /// <remarks>
+        /// Wykorzystując połączenie z bazą danych SQL, wysyła kwerendę liczącą ilość wierszy zawierających podany przez administratora tytuł filmu. Jeśli zwróci wartość większą niż 0, to znaczy, że 
+        /// film o podanym tytule już istnieje.
+        /// </remarks>
+        /// <param name="con">Odpowiada za połączenie z bazą danych, w formacie SqlConnection</param>
+        /// <param name="title">Odpowiada za tytuł filmu, w formacie string</param>
+        /// <returns></returns>
         public static int CheckExistingMovie(SqlConnection con, string title)
         {                        
             SqlCommand cmd = new SqlCommand();
@@ -55,6 +99,21 @@ namespace Search_Engine_Library
             return recordscheck;
         }
 
+        /// <summary>
+        /// Metoda statyczna AddNewMovie
+        /// Pobiera dane podane przez administratora w aplikacji dane, by dodać nowy film do bazy danych.
+        /// </summary>
+        /// <remarks>
+        /// Wykorzystując połączenie z bazą danych SQL, wysyła polecenie INSERT do bazy danych, tworząc w bazie nowy film zgodnie z podanymi danymi. Pobiera także z dysku plakat-zamiennik,
+        /// który zajmie miejsce prawdziwego plakatu (problemy z implementacją) i korzystając z metody GetPoster, zamienia go na tablicę byte.
+        /// </remarks>
+        /// <param name="con">Odpowiada za połączenie z bazą danych, w formacie SqlConnection</param>
+        /// <param name="title">Odpowiada za tytuł filmu, w formacie string</param>
+        /// <param name="price">Odpowiada za cenę filmu, w formacie string</param>
+        /// <param name="releasedate">Odpowiada za datę wydania filmu, w formacie string</param>
+        /// <param name="genre">Odpowiada za gatunek filmu, w formacie string</param>
+        /// <param name="language">Odpowiada za język filmu, w formacie string</param>
+        /// <param name="runtime">Odpowiada za czas trwania filmu, w formacie string</param>
         public static void AddNewMovie(SqlConnection con, string title, string price, string releasedate, string genre, string language, string runtime)
         {
             string path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"posters\", "default.jpg");
